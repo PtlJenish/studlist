841e1536284e86f2cfbcdb64a3945797d2bd68d7
PtlJenish <jenishpatel01191@gmail.com>
2022-01-25 09:41:47 -0500

    First Commit

---
+++ src/newstudlist/Newstudlist.java
@@ -0,0 +1,17 @@
+package newstudlist;
+
+/**
+ *
+ * This class +++ Insert class description here +++
+ * @author Jenish Patel
+ */
+public class Newstudlist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
